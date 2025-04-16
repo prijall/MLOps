85d9ba8bc267cc0ec6633548a76428d545a217fa
prijall <khadkaprijal@gmail.com>
2025-04-16 22:35:21 +0545

    failed creating pipeline

---
+++ Customer_Satisfaction_Project/run_pipeline.py
@@ -0,0 +1,4 @@
+from pipelines.training_pipeline import train_pipeline
+
+if __name__=="__main__":
+    train_pipeline(data_path="C:/Users/khadk/Desktop/MLOps/Customer_Satisfaction_Project/Data/olist_customers_dataset.csv")
