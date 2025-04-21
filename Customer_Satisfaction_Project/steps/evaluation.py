1b8bda848feaedec0768f8fce84521ce611e2f55
prijall <khadkaprijal@gmail.com>
2025-04-21 21:45:48 +0545

    steps for evaluation

--- Customer_Satisfaction_Project/steps/evaluation.py
+++ Customer_Satisfaction_Project/steps/evaluation.py
@@ -1,7 +1,28 @@
 import logging # keeps track of log activities
 import pandas as pd
 from zenml import step
+from src.evaluation import MSE, R2
+from sklearn.base import RegressorMixin
+from typing import Tuple
+from typing_extensions import Annotated
 
 @step
-def evaluate_model(df:pd.DataFrame)->None:
-    pass
+def evaluate_model(model:RegressorMixin, 
+                   X_test: pd.DataFrame, 
+                   y_test:pd.DataFrame)->Tuple[
+                      Annotated[float, 'mse'],
+                      Annotated[float, 'r2_score']   
+                   ]:
+         try:
+           prediction=model.predict(X_test)
+           mse_class=MSE()
+           mse=mse_class.calulate_score(y_test, prediction)
+
+           r2_class=R2()
+           r2=r2_class.calulate_score(y_test, prediction)
+
+           return mse, r2
+         
+         except Exception as e:
+               logging.error('Error: {}'.format(e))
+               raise e
