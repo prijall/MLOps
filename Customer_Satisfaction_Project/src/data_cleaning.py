d6d663b901aa51b1f4c8f19c18332d56486ab573
prijall <khadkaprijal@gmail.com>
2025-04-30 22:38:47 +0545

    updated code

--- Customer_Satisfaction_Project/src/data_cleaning.py
+++ Customer_Satisfaction_Project/src/data_cleaning.py
@@ -1,74 +1,99 @@
 import logging
-import numpy as np
-import pandas as pd
-from sklearn.model_selection import train_test_split
 from abc import ABC, abstractmethod
 from typing import Union
 
-#@ Creating an abstract class:
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
 class DataStrategy(ABC):
-     """
-     abstract for defining strategy for handling data
-     """
-     @abstractmethod
-     def handle_data(self, data:pd.DataFrame)->Union[pd.DataFrame, pd.Series]:
-          pass
-     
+    """
+    Abstract class defining strategy for handling data
+    """
+
+    @abstractmethod
+    def handle_data(self, data: pd.DataFrame) -> Union[pd.DataFrame, pd.Series]:
+        pass
+
 class DataPreProcessStrategy(DataStrategy):
-     def handle_data(self, data:pd.DataFrame)->pd.DataFrame:
-          """
-          preprocesses Data
-          """
-          try:
-            data=data.drop(
-               [
-                 "order_approved_at",
-                 "order_delivered_carrier_date",    
-                 "order_delivered_customer_date",
-                 "order_estimated_delivery_date",
-                 "order_purchase_timestamp"], axis=1)
-            
-            data["product_weight_g"] = data["product_weight_g"].fillna(data["product_weight_g"].median(), inplace=True)
-            data["product_length_cm"] = data["product_length_cm"].fillna(data["product_length_cm"].median(), inplace=True)
-            data["product_height_cm"] = data["product_height_cm"].fillna(data["product_height_cm"].median(), inplace=True)
-            data["product_width_cm"] = data["product_width_cm"].fillna(data["product_width_cm"].median(), inplace=True)
-            data["review_comment_message"] = data["review_comment_message"].fillna("No review", inplace=True)   
+    """
+    Data preprocessing strategy which preprocesses the data.    """
 
-            data=data.select_dtypes(include=[np.number])
-            cols_to_drop=["customer_zip_code_prefix", "order_item_id"]
-            data=data.drop(cols_to_drop, axis=1)
+    def handle_data(self, data: pd.DataFrame) -> pd.DataFrame:
+        """
+        Removes columns which are not required and filling missing values with median value
+        """
+        try:
+            data = data.drop(
+                [
+                    "order_approved_at",
+                    "order_delivered_carrier_date",
+                    "order_delivered_customer_date",
+                    "order_estimated_delivery_date",
+                    "order_purchase_timestamp",
+                ],
+                axis=1,
+            )
+
+            data["product_weight_g"].fillna(data["product_weight_g"].median(), inplace=True)
+            data["product_length_cm"].fillna(data["product_length_cm"].median(), inplace=True)
+            data["product_height_cm"].fillna(data["product_height_cm"].median(), inplace=True)
+            data["product_width_cm"].fillna(data["product_width_cm"].median(), inplace=True)
+            # write "No review" to fill missing values in review_comment_message column
+            data["review_comment_message"].fillna("No review", inplace=True)
+
+            # Select numerical columns only
+            data = data.select_dtypes(include=[np.number])
+            cols_to_drop = ["customer_zip_code_prefix", "order_item_id"]
+            data = data.drop(cols_to_drop, axis=1)
+
             return data
+        except Exception as e:
+            logging.error(f"Error in preprocessing data: {e}")
+            raise e
 
-          except Exception as e:
-              logging.error("Error in preprocessing data:{}".format(e))
-              raise e
-          
 class DataDivisionStrategy(DataStrategy):
-    def handle_data(self, data:pd.DataFrame)-> Union[pd.DataFrame, pd.Series]:
+    """
+    Data dividing/splitting strategy which divides/splites the data into train and test data.
+    """
+
+    def handle_data(self, data: pd.DataFrame) -> Union[pd.DataFrame, pd.Series]:
+        """
+        Divides the data into train and test data.
+        """
         try:
-            X=data.drop(['review_score'], axis=1)
-            y=data['review_score']
-            X_train, X_test, y_train, y_test=train_test_split(X, y, train_size=0.8, random_state=43)
+            X = data.drop("review_score", axis=1)
+            y = data["review_score"]
+            X_train, X_test, y_train, y_test = train_test_split(
+                X, y, test_size=0.2, random_state=42
+            )
             return X_train, X_test, y_train, y_test
-        
         except Exception as e:
-            logging.error("Error in data division:{}".format(e))
+            logging.error(f"Error in dividing data: {e}")
             raise e
 
+# It is the final class which will make use of all the strategies
 class DataCleaning:
-    def __init__(self, data:pd.DataFrame, strategy:DataStrategy):
-        self.data=data
-        self.stratgy=strategy
+    """
+    Data cleaning class preprocesses the data and divides/splits it into train and test data.
+    """
 
-    def handle_data(self)->Union[pd.DataFrame, pd.Series]:
+    def __init__(self, data: pd.DataFrame, strategy: DataStrategy) -> None:
+        """Initializes the DataCleaning class with a specific strategy."""
+        self.df = data
+        self.strategy = strategy
+
+    def handle_data(self) -> Union[pd.DataFrame, pd.Series]:
+        """Handle data based on the provided strategy"""
         try:
-            return self.strategy.handle_data(self.data)
+            return self.strategy.handle_data(self.df)
         except Exception as e:
-            logging.error("Error in handling data {}".format(e))
+            logging.error(f"Error in handling data: {e}")
             raise e
-    
-
         
-
+if __name__ == "__main__":
+    data = pd.read_csv('C:/Users/khadk/Desktop/MLOps/Customer_Satisfaction_Project/Data/olist_customers_dataset.csv')
+    data_cleaning = DataCleaning(data, DataPreProcessStrategy)
+    data_cleaning.handle_data()
               
 
--- Customer_Satisfaction_Project/src/model_dev.py
+++ Customer_Satisfaction_Project/src/model_dev.py
@@ -1,35 +1,48 @@
 import logging
 from abc import ABC, abstractmethod
+
 from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor
+
 
 class Model(ABC):
-   
+    """
+    Abstract class for all models
+    """
+
     @abstractmethod
     def train(self, X_train, y_train):
+        """
+        Trains the model
+
+        Args:
+            X_train: Training data
+            y_train: Training label
+        Returns:
+               None
+        """
         pass
 
-
 class LinearRegressionModel(Model):
-    
-    def train(self, X_train, y_train, **kwargs):
+    """
+    Linear Regression model
+    """
 
+    def train(self, X_train, y_train, **kwargs):
+        """
+        Trains the model
+
+        Args:
+            X_train: Training data
+            y_train: Training label
+        Returns:
+               None
+        """
         try:
-            reg=LinearRegression(**kwargs)
+            reg = LinearRegression(**kwargs)
             reg.fit(X_train, y_train)
-            logging.info('Model training completed')
-        
+            logging.info("Model training completed")
+            return reg
         except Exception as e:
-            logging.error('Error in training model:{}'.format(e))
+            logging.error(f"Error in training model {e}")
             raise e
-  
-class RandomForestModel(Model):
-    def train(X_train, y_train, **kwargs):
-        try:
-            rf=RandomForestRegressor()
-            rf.fit(X_train, y_train)
-            logging.info('Model training completed sucessfully')
-        
-        except Exception as e:
-            logging.error('Error while training;{}'.format(e))
-            raise e
+    
